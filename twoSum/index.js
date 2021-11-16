6de0dbc44f74ce60bfb49252292332bd36799863
Victor Souza <dsoouuzavitu@gmail.com>
2021-11-16 14:28:41 -0300

    add comments

--- twoSum/index.js
+++ twoSum/index.js
@@ -1,6 +1,10 @@
 const twoSum = function(nums, target) {
+    
+    // Percorre o array, i recebe o primeiro número
     for ( let i = 0; i < nums.length; i++ ) {
+        // Percorre o array novamente, index recebe o segundo número
         for ( let index = 0; index < nums.length; index++ ) {
+            // Se forem o mesmo índice do array, será ignorado
             if ( i === index ) continue
             if ( nums[i] + nums[index] === target ) {
                 return [i, index]
